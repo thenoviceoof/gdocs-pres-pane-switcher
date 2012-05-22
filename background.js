7785e4a7ce58bb58287e97c29fcc648c1d98dcfb
Nathan Hwang <beta.entity.k@gmail.com>
2012-05-21 20:42:20 -0700

    Putting everything together

--- background.js
+++ background.js
@@ -6,3 +6,19 @@ function check(tabId, changeInfo, tab) {
 }
 
 chrome.tabs.onUpdated.addListener(check);
+
+var state_active = false;
+chrome.pageAction.onClicked.addListener(function(tab) {
+    state_active = !state_active;
+    var path_dict = {false: "icon_default.png", true: "icon_active.png"};
+    var path = path_dict[state_active];
+    chrome.pageAction.setIcon({
+        "tabId": tab.id,
+        "path": path
+    });
+    if(state_active) {
+        chrome.tabs.executeScript(null, {file: "pane_hide.js"});
+    } else {
+        chrome.tabs.executeScript(null, {file: "pane_show.js"});
+    }
+});
